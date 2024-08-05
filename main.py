cc1c37e977eed0514a683b4b9e18dfb719a9adf9
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-08-04 23:08:08 -0600

    Basics of wEB SCRAPING

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from bs4 import BeautifulSoup
 
+countryInfo = {}
+
 with open("SimpleScraper.html") as file:
     content = file.read()
 
 soup = BeautifulSoup(content, "lxml")
-tags = soup.find_all("div")
+country_div = soup.find_all("div", class_ = "row")
 
-for div in tags:
-    print(div)
+for div in country_div:
+    if div.h3:
+        name = div.h3.text.strip()
+        info = div.find("div", class_ = "country-info")
+        
+        if info:
+            countryDict = {}
+            info = info.text.strip()
+            info = info.split("\n")
+            
+            for data in info:
+                data = data.split(": ")
+                countryDict[data[0]] = data[1]
+            
+            countryInfo[name] = countryDict
+           
+
+print(countryInfo)
