f5234070d89391862cdb3dc61f8e8c83801210d0
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-08-14 23:13:49 -0600

    .

--- amazon.py
+++ amazon.py
@@ -10,6 +10,11 @@ driver = webdriver.Chrome()
 link = "https://www.amazon.ca/s?k"
 driver.get(link)
 
+
+searchBar = driver.find_element(By.ID, "f")
+searchBar.click()
+searchBar.send_keys("vibrator")
+
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 print(soup)
