aa268bb401186a94b202d481e26f02dd33f14a99
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-08-24 18:27:41 -0600

    .

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from amazon import getWishlistData, getDataLink
+from bestbuy import getItemData
 
-what = input("do you want to import a wishlist or singular item?(Answer W for Wishlist or S for SIngular item):\n")
+# what = input("do you want to import a wishlist or singular item?(Answer W for Wishlist or S for SIngular item):\n")
 
-if what.upper() == "W":
-    link = input("Paste in the link to your wishlist:\n")
-    while True:
+# if what.upper() == "W":
+#     link = input("Paste in the link to your wishlist:\n")
+#     while True:
+#         try:
+#             wishlist = getWishlistData(link)
+#             break
+#         except Exception as e:
+#             print(e)
+#     print("Wishlist saved!")
+#     items = wishlist
+# else:
+#     link = input("Paste in the link to your item:\n")
+#     while True:
+#         try:
+#             itemData = getDataLink(link)
+#             break
+#         except Exception as e:
+#             print(e)
+#     print("Item saved!")
+#     items = itemData
+    
+    
+# print(items)
+
+while True:
         try:
-            wishlist = getWishlistData(link)
+            itemData = getItemData(link)
             break
         except Exception as e:
             print(e)
-    print("Wishlist saved!")
-    items = wishlist
-else:
-    link = input("Paste in the link to your item:\n")
-    while True:
-        try:
-            itemData = getDataLink(link)
-            break
-        except Exception as e:
-            print(e)
-    print("Item saved!")
-    items = itemData
-    
-    
-print(items)
+print("Wishlist saved")
+
