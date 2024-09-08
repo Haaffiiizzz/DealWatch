f8f437a58bc78ab460144a5d0b5afddac0280d07
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-09-08 11:45:30 -0500

    Get image src for bestbuy

--- app/scrapers/bestbuyScrape.py
+++ app/scrapers/bestbuyScrape.py
@@ -1,20 +1,26 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def getItemData(itemLink):
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
-    driver = webdriver.Chrome(options = chrome_options)
+    driver = webdriver.Chrome(options = chrome_options) #options = chrome_options
     
     driver.get(itemLink)
-
+    
+    price_tag = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '[data-automation="product-price"]')))
+        
     soup = BeautifulSoup(driver.page_source, "html.parser")
 
     nameTag = soup.find("h1", {"class": "font-best-buy text-body-lg font-medium sm:text-title-sm"})
     brandTag = soup.find("a", {"data-automation": "pdp-brandname-link"})
     priceTag = soup.find("span", {"data-automation": "product-price"})
+    imageTag = soup.find("img", {"class":"productImage_1NbKv"})
     
 
     wishlist = []
@@ -22,23 +28,10 @@ def getItemData(itemLink):
     Dict["Item"] = nameTag.text.strip() if nameTag else None
     Dict["Brand"] = brandTag.text.strip() if brandTag else None
     Dict["Price"] = priceTag.text.strip().split("$")[1] if priceTag else None
+    Dict["ImageSrc"] = imageTag.get('src') if imageTag else None
 
     wishlist.append(Dict)
 
     return Dict
 
-# def searchItem(itemName):
-#     headers = {
-#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
-# }
-#     page = requests.get(f"https://www.bestbuy.ca/en-ca/search?search={itemName}", headers=headers)
-    
-#     soup = BeautifulSoup(page.content, "html.parser")
-#     print(soup)
-    
-# print(searchItem("Samsung Odyssey"))
-    
-    
-
-
-
+# print(getItemData('https://www.bestbuy.ca/en-ca/product/razer-basilisk-v3-26000-dpi-optical-gaming-mouse-black/15688110'))
