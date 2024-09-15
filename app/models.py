5ce4ed3e560e1e344ee362107ec6aa0395eb24e5
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-09-14 22:08:48 -0500

    bruh

--- app/models.py
+++ app/models.py
@@ -1,23 +1,26 @@
 from database import Base
-from sqlalchemy import Column, String, Integer, JSON, TIMESTAMP, text
+from sqlalchemy import Column, String, Integer, JSON, TIMESTAMP, text, Boolean
 
-class Amazon(Base):
-    __tablename__ = "Amazon"
-    country = Column(String, primary_key=True, unique=True)
-    items = Column(JSON)
+# class Amazon(Base):
+#     __tablename__ = "Amazon"
+#     UserID = Column(Integer, primary_key=True, nullable=False)
+    
 
-class BestBuy(Base):
-    __tablename__ = "BestBuy"
-    country = Column(String, primary_key=True, unique=True)
-    items = Column(JSON)
+# class BestBuy(Base):
+#     __tablename__ = "BestBuy"
+#     country = Column(String, primary_key=True, unique=True)
+#     items = Column(JSON)
 
 
 class User(Base):
     __tablename__ = "users"
+    id = Column(Integer, primary_key= True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    id = Column(Integer, primary_key= True, nullable=False)
+    amazon = Column(Boolean, server_default = 'False')
+    bestbuy = Column(Boolean, server_default = 'False')
     createdAt = Column(TIMESTAMP(timezone=True), nullable=False, 
                        server_default= text('now()'))
+    
 
 # here we create the model for the countries and users table to be used with the db query
