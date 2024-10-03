c76c7f90e0037395c4a54a904713a2d24e6ffcea
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-10-03 02:43:01 -0500

    Can now add a user's wishlist to database

--- app/routers/amazon.py
+++ app/routers/amazon.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from urllib.parse import unquote
 from ..scrapers.amazonScrape import getWishlistData, getDataLink
 from ..oauth2 import getCurrentUser
 from ..database import get_db
 from sqlalchemy.orm import Session
 from ..schemas import TokenData
+from .. import models
 
 router = APIRouter(prefix = "/amazon", tags= ["Amazon"]) 
 
@@ -12,11 +13,35 @@ router = APIRouter(prefix = "/amazon", tags= ["Amazon"])
 def root(currUser: TokenData = Depends(getCurrentUser), db: Session = Depends(get_db)):
     return currUser.id
 
-@router.get("/wishlist")
-def wishlist(link: str):
+@router.get("/wishlist")   #change this to post soon
+def wishlist(link: str, currUser: TokenData = Depends(getCurrentUser), db: Session = Depends(get_db)):
     # we get link by adding a query to the wishlist path
     link = unquote(link)
-    return getWishlistData(link)
+    scrapedData = getWishlistData(link)
+    
+    if not scrapedData:
+        raise HTTPException(status_code=404, detail="No data found at the provided link.")
+
+    for item in scrapedData:
+        title = item.get("Item")
+        brand = item.get("Brand")
+        price = item.get("Price")
+        imageSrc = item.get("ImageSrc")
+
+        wishlist = models.Amazon(
+            userId=currUser.id,
+            title=title,
+            brand=brand,
+            price=price,
+            imageSrc=imageSrc
+        )
+
+        db.add(wishlist)
+
+    db.commit() 
+
+    return {"message": "Wishlist items added successfully", "items": scrapedData}
+    
 
 @router.get("/itemlink")
 def itemLink(link: str):
