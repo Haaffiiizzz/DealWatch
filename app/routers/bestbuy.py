f77ef1447ae2e08b50d079a7507f7487d75545ab
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-10-17 23:39:11 -0500

    BEstBuy adding items. Need testing

--- app/routers/bestbuy.py
+++ app/routers/bestbuy.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
-from urllib.parse import unquote
 from ..scrapers.bestbuyScrape import getItemData
+from fastapi import APIRouter, Depends, HTTPException
+from urllib.parse import unquote
+from ..oauth2 import getCurrentUser
+from ..database import get_db
+from sqlalchemy.orm import Session
+from ..schemas import TokenData
+from .. import models
 
 router = APIRouter(prefix = "/bestbuy", tags= ["BestBuy"]) 
 
@@ -10,8 +15,33 @@ def root():
     return "BestBuy"
 
 @router.get("/itemlink")
-def itemLink(link: str):
+def itemLink(link: str, currUser: TokenData = Depends(getCurrentUser), db: Session = Depends(get_db)):
     # we get link by adding a query to the itemlink path
     link = unquote(link)
-    return getItemData(link)
+    scrapedData = getItemData(link)
+    
+    if not scrapedData:
+        raise HTTPException(status_code=404, detail="No data found at the provided link.")
+
+    for item in scrapedData:
+        title = item.get("Item")
+        brand = item.get("Brand")
+        price = item.get("Price")
+        imageSrc = item.get("ImageSrc")
+
+        wishlist = models.BestBuy(
+            userId=currUser.id,
+            title=title,
+            brand=brand,
+            price=price,
+            imageSrc=imageSrc
+        )
+
+        db.add(wishlist)
+
+    db.commit() 
+
+    return {"message": "Wishlist items added successfully", "items": scrapedData}
+    # code copied from amazon router. test tomorrow to make sure it works.
+    
 
