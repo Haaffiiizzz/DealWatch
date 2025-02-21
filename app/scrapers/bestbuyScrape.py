7094cca1ea9845c8798cf2dc54d160db8f5953d1
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-02-21 12:50:34 -0600

    Chrome Driver Buildpack

--- app/scrapers/bestbuyScrape.py
+++ app/scrapers/bestbuyScrape.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
+import os
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
@@ -78,10 +79,16 @@ def getSearchData(searchTerm: str):
     searchLink = f"https://www.bestbuy.ca/en-ca/search?search={searchTerm}"
     
     options = ChromeOptions()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
-    DRIVER = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) #service=Service(ChromeDriverManager().install()), 
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    service = Service(os.environ.get("CHROMEDRIVER_PATH"))
+    
+    DRIVER = webdriver.Chrome(service=service, options=options)
+    
     DRIVER.get(searchLink)
     
     
